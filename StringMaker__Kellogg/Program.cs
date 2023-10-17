40a6c54ef80eac5f5573f3b34d76e1cedf7d7c7f
zitanicole <zita.kellogg@student.centralia.edu>
2023-10-17 01:00:25 -0700

    Add project files.

---
+++ StringMaker__Kellogg/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace StringMaker__Kellogg
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			StringManager sm = new StringManager("input");
+			string reversed = sm.Reverse("Sunbeam Tiger");
+			Console.WriteLine("Reversed String: " + reversed);
+			
+		}
+	}
+}
---
+++ StringMaker__Kellogg/StringManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMaker__Kellogg
+{
+	internal class StringManager
+	{
+		private string _input;
+
+		public StringManager(string input)
+		{
+			_input = input;	
+		}
+
+		public string Reverse(string s) 
+		{
+			char[] chars = s.ToCharArray();
+			Stack<char> stack = new Stack<char>();
+			foreach (char c in chars)
+			{
+				stack.Push(c);
+			}
+
+			char[] reversed = new char[chars.Length];
+			int i = 0;	
+			while(stack.Count > 0)
+			{
+				reversed[i] = stack.Pop();
+				i++;
+			}
+
+			return new string(reversed);
+		}
+
+	}
+}
